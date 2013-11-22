04094022b86782c6506b813899fdfc78fc0fbd06
mco67 <cordebard@gmail.com>
2013-11-22 21:47:23 +0100

    Finalize coreData mechanism

--- RayNov/RayNov/RNAppDelegate.h
+++ RayNov/RayNov/RNAppDelegate.h
@@ -10,6 +10,9 @@
 
 @interface RNAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIWindow* window;
+@property (readonly, strong, nonatomic) NSManagedObjectContext* managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel* managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator* persistentStoreCoordinator;
 
 @end
--- RayNov/RayNov/RNViewController.h
+++ RayNov/RayNov/RNViewController.h
@@ -10,4 +10,6 @@
 
 @interface RNViewController : UIViewController
 
+@property (nonatomic, strong) NSManagedObjectContext* managedObjectContext;
+
 @end
